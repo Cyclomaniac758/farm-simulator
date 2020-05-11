e9b9544bffa50e31cdde60fc4987cbc1add9b639
Icarus <Icarus@192.168.20.4>
2020-05-12 11:33:30 +1200

    New game

--- FarmSimulator/src/game/Game.java
+++ FarmSimulator/src/game/Game.java
@@ -1,5 +1,38 @@
 package game;
 
+import java.util.Scanner;
+import farm.*;
+import farmer.*;
+
 public class Game {
 	
+	
+	public void startGame() {
+		Farm farm = this.createFarm();
+		System.out.println("done");
+	}
+	
+	public Farm createFarm() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Choose your farm type by entering the respective number./n1. Family Farm\n2. Commercial Farm\n"
+				+ "3. Livestock Farm\n4. Crop Farm");
+		int num = scan.nextInt();
+		System.out.println("Enter your farm name");
+		String name = scan.nextLine();
+		if (num == 1) {
+			FamilyFarm farm = new FamilyFarm(name);
+		} else if (num == 2) {
+			CommercialFarm farm = new CommercialFarm(name);
+		} else if (num == 3) {
+			LivestockFarm farm = new LivestockFarm(name);
+		} else if (num == 4) {
+			OrchardFarm farm = new OrchardFarm(name);
+		}
+		return farm;
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.startGame();
+	}
 }
